861fb9afa41f6b21d479b295fd8e659c97ff44ca
yuv-raj-25 <yuvrajchaudhary464@gmail.com>
2026-02-18 20:50:48 +0530

    added prodcution url of the backend map to url

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,5 +1,5 @@
 // Point this to your backend server
-const BACKEND_URL = import.meta.env.VITE_BACKEND_URL || "https://emitrr-connect-4.onrender.com";
+const BACKEND_URL = import.meta.env.VITE_BACKEND_URL || "http://localhost:5000";
 
 export const WS_URL = BACKEND_URL.replace(/^http/, "ws");
 export const API_URL = BACKEND_URL;
